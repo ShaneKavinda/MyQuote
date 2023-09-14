91e2dc96cddbea1c28a12ac48ea8bc728adc30c5
ShaneKavinda1 <skdalumu@our.ecu.edu.au>
2023-09-14 14:21:51 +0800

    fixed a syntax error on quote.rb

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ApplicationRecord
   belongs_to :user
   belongs_to :philosopher
   has_many :quote_categories, dependent: :destroy
-  has_many :categories, through: quote_categories
+  has_many :categories, through: :quote_categories
 end
