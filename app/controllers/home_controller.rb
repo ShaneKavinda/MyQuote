729f74b0d54126c4b50d229345041449b827b3ac
ShaneKavinda <shanekavinda1999@gmail.com>
2023-10-17 00:57:13 +0800

    added user authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     @quotes = Quote.includes(:categories).take(5)
   end
+
+  def uquotes
+    @quotes = Quote.includes(:categories).where(user_id: session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ Rails.application.routes.draw do
   root "home#index"
   get '/about', to: 'about#index'
   get 'search', to: 'search#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get '/admin', to: 'home#aindex'
+  get '/userhome', to: 'home#uindex'
+  get '/your-quotes', to: 'home#uquotes'
+
 end
