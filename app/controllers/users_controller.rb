62121b22e5445482220da7753f0643f2e9dc84c0
ShaneKavinda <shanekavinda1999@gmail.com>
2023-10-16 21:20:13 +0800

    bug fix with adding new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :is_admin, :status, :email, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :is_admin, :status, :email, :password)
     end
 end
